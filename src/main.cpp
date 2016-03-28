84358a2ec42416ae63d7efad991efd799b9bddf4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2016-03-27 21:11:16 -0300

    Added gen_key method to gen random key and..
    Added ability to provide key or not when encrypting

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,23 @@ void usage()
 {
 	printf("usage: ./main [OPTIONS...] in_file out_file\n");
 	printf("\nOptions:\n");
-	printf("\t-d key     Decrypt in_file and put the decryption result in out_file\n");
-	printf("\t-e key     Encrypt in_file and put the encryption result in out_file\n");
+	printf("\t-d key     Decrypt in_file with key=key and put the decryption result in out_file\n");
+	printf("\t-e         Encrypt in_file and put the encryption result in out_file\n");
+}
+
+void gen_key(char key[17])
+{
+	const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 17; i++)
+	{
+		key[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+	}
+
+	key[16] = 0;
 }
 
 void decrypt(string in, string out, const char* key)
@@ -48,14 +63,42 @@ void encrypt(string in, string out, const char* key)
 int main(int argc, char* argv[])
 {
 	// ensure proper usage
-	if (argc != 5)
+	if (argc < 4 || argc > 5)
 	{
 		usage();
 		return 1;
 	}
 
-	string in_file = argv[3];
-	string out_file = argv[4];
+	// key to encrypt and decrypt
+	char key[17];
+
+	// get command line arguments
+	char c;
+	while ((c = getopt(argc, argv, "ed:")) != -1)
+	{
+		switch (c)
+		{
+			case 'e':
+				ENCRYPT = true;
+				break;
+			case 'd':
+				// check for proper usage
+				if (argc != 4 || strlen(optarg) != 16)
+				{
+					fprintf(stderr, "In decryption mode, one must give a 16 bytes long key.\n");
+					usage();
+					return 1;
+				}
+				strncpy(key, optarg, 16);
+				break;
+			default:
+				usage();
+				return 1;
+		}
+	}
+
+	string in_file = argv[optind++];
+	string out_file = argv[optind];
 
 	// check for file existence
 	if (access(in_file.c_str(), F_OK) == -1)
@@ -65,30 +108,9 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	// key to encrypt and decrypt
-	char* key;
-
-	// get command line arguments
-	char c;
-	while ((c = getopt(argc, argv, "e:d:")) != -1)
-	{
-		switch (c)
-		{
-			case 'e':
-				ENCRYPT = true;
-				key = optarg;
-				break;
-			case 'd':
-				key = optarg;
-				break;
-			default:
-				usage();
-				return 1;
-		}
-	}
-
 	if (ENCRYPT)
 	{
+		gen_key(key);
 		encrypt(in_file, out_file, key);
 	}
 	else
